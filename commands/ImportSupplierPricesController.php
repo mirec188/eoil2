19aa61b224e4065c56571340f868d2594294098d
mirec <kanovskym@gmail.com>
2021-04-25 13:36:51 +0200

    totok

---
+++ commands/ImportSupplierPricesController.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ImportSupplierPricesController extends Controller
+{
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     * @return int Exit code
+     */
+    public function actionIndex($path)
+    {   
+        $data = $this->parseCsvFile($path);
+        $this->import($data);
+    }
+
+
+    private function import($data) {
+        foreach ($data as $row) {
+            $pp = \app\models\ProductPack::find($row['eId']);
+            
+        }
+    }
+
+    private function parseCsvFile($file) {
+
+        $return = array();
+        $handle = fopen($file, "r");
+        if ($handle) {
+            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+               $tmp['eId'] = $data[0];
+               $tmp['price'] = $this->sanitazePrice($data[1]);
+               $return[] = $tmp;
+            }
+            fclose($handle);
+            return $return;
+        }
+        return false;
+    }
+
+    private function sanitazePrice($price) {
+        return str_replace(',', '.', $price);
+    }
+
+}
